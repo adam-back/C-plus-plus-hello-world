e97056e90d6eb6f73fa4a61a02c020147e51d86e
Adam Back <adb33@email.arizona.edu>
2015-08-15 10:17:33 -0700

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Adam Back on 8/15/15.
+//  Copyright (c) 2015 Adam Back. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
