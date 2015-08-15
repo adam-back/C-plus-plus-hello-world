c3b5add3eead4d010d2bb3d403177f4e0b3e9afa
Adam Back <adb33@email.arizona.edu>
2015-08-15 10:30:52 -0700

    (cleanup) Removed unneeded main fn code.

--- Hello World/main.cpp
+++ Hello World/main.cpp
@@ -8,8 +8,8 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!";
     return 0;
 }
